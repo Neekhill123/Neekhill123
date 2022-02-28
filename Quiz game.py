eb3ce15128d705974d3cc4cf3e607739511d6b1b
Neekhill123 <100573842+Neekhill123@users.noreply.github.com>
2022-02-28 17:08:06 +0545

    Add files via upload

---
+++ Quiz game.py
@@ -0,0 +1,49 @@
+#Let's make a quiz game
+
+print("Hello!Welcome to my Quiz show")
+
+quiz=input("Do you want to play my quiz game: ")
+if quiz=="yes":
+    print("Alright Let's get started.")
+else:
+    print("Okay!I value your time")
+
+question_1=input("What is the capital of England?: ")
+if question_1=="London":
+    print("You got it right")
+else:
+    print("You are horrible in geography")
+
+question_2=input("Who is the richest person of the world?: ")
+if question_2=="Elon Musk":
+    print("You got it right")
+else:
+    print("You are horrible in finance")    
+
+question_3=input(" Who won the NBA last year? : ")
+if question_3=="Suns":
+    print("You got it right")
+else:
+    print("You are horrible in sports")
+
+
+
+question_4=input("What is the original name of Harvey Specter?: ")
+if question_4=="Gabriel Macht":
+    print("You got it right")
+else:
+    print("You are shit in watching web series..")
+
+
+question_5=input("Who is the girlfriend of Mike Ross?: ")
+if question_5=="Rachel Zane":
+    print("You got it right")
+else:
+    print("You are horrible in movies..")
+
+print("Thank you for your gracious effort..")    
+
+
+
+
+            
